e398b631e597e0861fb89b5969c4a0a99dbe108f
tomay <tom.allnutt@gmail.com>
2014-01-14 10:07:11 +0200

    simple script to remove and copy dev files

---
+++ dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "removing map.html"
+rm -i map.html
+echo "removing view.js"
+rm -i javascript/view.js
+echo "copying dev files"
+cp map-dev.html map.html
+cp javascript/view-dev.js javascript/view.js
