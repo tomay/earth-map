8649bb95ea7659ae052d13c5a26abf8fdc610336
tomay <tom.allnutt@gmail.com>
2014-01-14 10:10:15 +0200

    replace text with sed

--- dev.sh
+++ dev.sh
@@ -6,3 +6,5 @@ rm -i javascript/view.js
 echo "copying dev files"
 cp map-dev.html map.html
 cp javascript/view-dev.js javascript/view.js
+echo "replacing text in map.html"
+sed -i 's/view-dev.js/view.js/g' map.html
